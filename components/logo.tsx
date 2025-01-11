964b03a2fe90d8f2e4fc2d7af85e80e1aa0d45bb
ads490707 <slightly123@gmail.com>
2025-01-10 22:52:52 -0500

    logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -6,11 +6,11 @@ export function Logo() {
     <Link href="/" className="flex items-center gap-3">
       <div className="w-10 h-10 rounded-lg bg-[#4ade80] flex items-center justify-center">
         <Image 
-          src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/e-bot_head%202.PNG-L0fuDlm3JtQHS08e0hRUgtz0rLpMGu.png"
+          src="/e-bot_profile.png"
           alt="Ebot Logo" 
           width={32} 
           height={32}
-          className="brightness-0 invert"
+          className=""
         />
       </div>
       <div className="flex flex-col">
