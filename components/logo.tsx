f031956a04e7a1176f5882db5713138c749d4835
ads490707 <slightly123@gmail.com>
2025-01-12 18:45:45 -0500

    Update logo.tsx

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-3">
+    <Link href="/" className="w-40 flex items-center gap-3">
       <div className="w-10 h-10 rounded-lg bg-[#4ade80] flex items-center justify-center">
         <Image 
           src="/e-bot_head.png"
