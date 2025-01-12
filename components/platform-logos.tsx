24e7307aee27d9558f0d67289c28a84400c3f9d6
ads490707 <slightly123@gmail.com>
2025-01-11 20:51:57 -0500

    Update platform-logos.tsx

--- components/platform-logos.tsx
+++ components/platform-logos.tsx
@@ -28,7 +28,7 @@ export function PlatformLogos() {
               alt={platform.name}
               height={40}
               width={40}
-              className="h-10 w-auto object-contain bg-transparent brightness-200 invert opacity-70 hover:opacity-100 transition-opacity"
+              className="h-8 w-auto object-contain bg-transparent brightness-200 invert opacity-70 hover:opacity-100 transition-opacity"
             />
           </div>
         ))}
