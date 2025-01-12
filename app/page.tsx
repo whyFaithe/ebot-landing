f4782c457c3d0a80d1c2ebdb3068ddee87d51fb6
ads490707 <slightly123@gmail.com>
2025-01-12 01:12:03 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
         <section id="features" className="relative mx-auto ">
           {/* Background Image */}
           <div
-            className="z-0 absolute opacity-30 inset-0"
+            className="z-0 absolute opacity-30 inset-0 pointer-events-none"
             style={{
               backgroundImage: `url('/background_lines2.svg')`,
               backgroundSize: 'contain',
@@ -125,7 +125,7 @@ export default function Home() {
         <section id="use-cases" className="relative mx-auto ">
           {/* Background Image */}
           <div
-            className="z-0 absolute opacity-30 inset-0"
+            className="z-0 absolute opacity-30 inset-0 pointer-events-none"
             style={{
               backgroundImage: `url('/background_lines3.svg')`,
               backgroundSize: 'contain',
@@ -161,7 +161,7 @@ export default function Home() {
         <section id="testimonials" className="relative  mx-auto ">
           {/* Background Image */}
           <div
-            className="z-0 absolute opacity-30 inset-0"
+            className="z-0 absolute opacity-30 inset-0 pointer-events-none"
             style={{
               backgroundImage: `url('/background_lines4.svg')`,
               backgroundSize: 'contain',
@@ -195,7 +195,7 @@ export default function Home() {
           
           {/* Background Image */}
           <div
-            className="z-0 absolute opacity-30 inset-0"
+            className="z-0 absolute opacity-30 inset-0 pointer-events-none"
             style={{
               backgroundImage: `url('/background_lines5.svg')`,
               backgroundSize: 'contain',
