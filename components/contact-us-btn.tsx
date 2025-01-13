c8334290cdf877e997f6c1cfb76f7b6212c87804
ads490707 <slightly123@gmail.com>
2025-01-12 19:59:57 -0500

    Update contact-us-btn.tsx

--- components/contact-us-btn.tsx
+++ components/contact-us-btn.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import Script from 'next/script';
 
 export function ContactUsBtn() {
@@ -11,25 +11,6 @@ export function ContactUsBtn() {
       feedbackInstance.current.open();
     }
   };
-
-  useEffect(() => {
-    // @ts-ignore
-    feedbackInstance.current = new window.JotformFeedback({
-      formId: '250055040868959',
-      base: 'https://whyfaithe.jotform.com/',
-      windowTitle: 'Contact Us',
-      backgroundColor: '#1fc55c',
-      fontColor: '#FFFFFF',
-      type: 'false',
-      height: 500,
-      width: 700,
-      openOnLoad: false, // Ensure it does not open on load
-    });
-
-    return () => {
-      // Clean up if necessary
-    }
-  }, [])
 
 
   return (
