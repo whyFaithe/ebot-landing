98443333d21b2574f49911e21fe4fb92ed4f0f35
ads490707 <slightly123@gmail.com>
2025-01-12 21:07:06 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ContactModal } from '@/components/contact-modal';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,11 +26,37 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          src="https://whyfaithe.jotform.com/static/feedback2.js"
+          type="text/javascript"
+        ></script>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var JFL_250055040868959 = new JotformFeedback({
+                formId: '250055040868959',
+                base: 'https://whyfaithe.jotform.com/',
+                windowTitle: 'Contact Us',
+                backgroundColor: '#1fc55c',
+                fontColor: '#FFFFFF',
+                type: 'false',
+                height: 500,
+                width: 700,
+                openOnLoad: false,
+              });
+            `,
+          }}
+        ></script>
+        <script
+          src="https://whyfaithe.jotform.com/s/umd/latest/for-form-embed-handler.js"
+        ></script>
+      </Head>
       <body className={inter.className}>
         {children}
 
         {/* Add the ContactModal at the end of the body */}
-        <ContactModal />
       </body>
     </html>
   );
