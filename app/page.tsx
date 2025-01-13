205886dfe7a2f4014a5fb94ff5bc20d46b5dc180
ads490707 <slightly123@gmail.com>
2025-01-12 21:18:43 -0500

    cursor on button

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
             <a 
                className="btn lightbox-250055040868959 
                bg-green-500 text-white hover:bg-green-600 
-               shadow-lg shadow-green-500/20 px-4 py-2 rounded"
+               shadow-lg shadow-green-500/20 px-4 py-2 rounded cursor-pointer"
             >
                 Contact Us 
              </a>
--- components/mobile-menu.tsx
+++ components/mobile-menu.tsx
@@ -61,7 +61,13 @@ export function MobileMenu() {
               >
                 Use Cases
               </Link>
-              <ContactUsBtn/>
+              <a 
+               className="btn lightbox-250055040868959 
+               bg-green-500 text-white hover:bg-green-600 
+               shadow-lg shadow-green-500/20 px-4 py-2 rounded cursor-pointer"
+            >
+                Contact Us 
+             </a>
             </nav>
           </div>
         </div>
