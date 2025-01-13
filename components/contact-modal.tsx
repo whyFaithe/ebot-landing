d4b9d46e123bcc20aa74cac74eb468f5006172c2
ads490707 <slightly123@gmail.com>
2025-01-12 20:31:07 -0500

    Update contact-modal.tsx

--- components/contact-modal.tsx
+++ components/contact-modal.tsx
@@ -1,27 +1,34 @@
+'use client';
+
+import { useEffect } from 'react';
 import Script from 'next/script';
 
 export function ContactModal() {
+  useEffect(() => {
+    // Initialize the JotformFeedback instance globally
+    window.jotformFeedbackInstance = new window.JotformFeedback({
+      formId: '250055040868959',
+      base: 'https://whyfaithe.jotform.com/',
+      windowTitle: 'Contact Us',
+      backgroundColor: '#1fc55c',
+      fontColor: '#FFFFFF',
+      type: 'false',
+      height: 500,
+      width: 700,
+      openOnLoad: false, // Ensure it doesn't open on load
+    });
+
+    return () => {
+      // Cleanup if necessary
+      delete window.jotformFeedbackInstance;
+    };
+  }, []);
+
   return (
     <>
-      {/* Load Feedback and Embed Scripts */}
-      <Script src="https://whyfaithe.jotform.com/static/feedback2.js" strategy="lazyOnload" />
-      <Script src="https://whyfaithe.jotform.com/s/umd/latest/for-form-embed-handler.js" strategy="lazyOnload" />
-      <Script id="jotform-feedback-init" strategy="lazyOnload">
-        {`
-          var JFL_250055040868959 = new JotformFeedback({
-            formId: '250055040868959',
-            base: 'https://whyfaithe.jotform.com/',
-            windowTitle: 'Contact Us',
-            backgroundColor: '#1fc55c',
-            fontColor: '#FFFFFF',
-            type: 'false',
-            height: 500,
-            width: 700,
-            openOnLoad: false
-          });
-        `}
-      </Script>
+      {/* Load JotForm Scripts */}
+      <Script src="https://whyfaithe.jotform.com/static/feedback2.js" strategy="beforeInteractive" />
+      <Script src="https://whyfaithe.jotform.com/s/umd/latest/for-form-embed-handler.js" strategy="beforeInteractive" />
     </>
   );
 }
-
