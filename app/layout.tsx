122ef924aeaecdfc09de529b868466a16841ef84
ads490707 <slightly123@gmail.com>
2025-02-12 06:55:01 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Head from 'next/head';
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] });
 
